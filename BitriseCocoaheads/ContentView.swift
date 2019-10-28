ae1df3a54bc535c5abf8d32ef5a2ff367339bbc6
Antoine Barrault <barrault01@gmail.com>
2019-10-27 21:23:55 -0300

    Initial Commit

---
+++ BitriseCocoaheads/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitriseCocoaheads
+//
+//  Created by Antoine Barrault on 27/10/19.
+//  Copyright © 2019 Guru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
